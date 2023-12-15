347f09aebda08d78321741c81bb5b7381b762f9a
Junichi Kato <j5ik2o@gmail.com>
2023-12-15 21:42:43 +0900

    add impls.

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -10,6 +10,11 @@ fn offer(c: &mut Criterion) {
   let mut vec_deque = queue_rs::queue::create_queue(QueueType::VecDequeue, QueueSize::Limitless);
   let mut queue_linked_list = queue_rs::queue::create_queue(QueueType::LinkedList, QueueSize::Limitless);
   let mut queue_mpsc = queue_rs::queue::create_queue(QueueType::MPSC, QueueSize::Limitless);
+  let mut vec_deque_blocking =
+    queue_rs::queue::create_queue(QueueType::VecDequeue, QueueSize::Limitless).with_blocking();
+  let mut queue_linked_list_blocking =
+    queue_rs::queue::create_queue(QueueType::LinkedList, QueueSize::Limitless).with_blocking();
+  let mut queue_mpsc_blocking = queue_rs::queue::create_queue(QueueType::MPSC, QueueSize::Limitless).with_blocking();
   let op = 1;
   group.bench_with_input(BenchmarkId::new("vec_deque", op), &op, |b, i| {
     b.iter(|| vec_deque.offer(*i))
@@ -20,6 +25,15 @@ fn offer(c: &mut Criterion) {
   group.bench_with_input(BenchmarkId::new("queue_mpsc", op), &op, |b, i| {
     b.iter(|| queue_mpsc.offer(*i))
   });
+  group.bench_with_input(BenchmarkId::new("vec_deque_blocking", op), &op, |b, i| {
+    b.iter(|| vec_deque_blocking.offer(*i))
+  });
+  group.bench_with_input(BenchmarkId::new("linked_list_blocking", op), &op, |b, i| {
+    b.iter(|| queue_linked_list_blocking.offer(*i))
+  });
+  group.bench_with_input(BenchmarkId::new("queue_mpsc_blocking", op), &op, |b, i| {
+    b.iter(|| queue_mpsc_blocking.offer(*i))
+  });
 }
 
 fn poll(c: &mut Criterion) {
@@ -29,6 +43,12 @@ fn poll(c: &mut Criterion) {
   let mut queue_linked_list =
     queue_rs::queue::create_queue_with_elements(QueueType::LinkedList, values.clone().into_iter());
   let mut queue_mpsc = queue_rs::queue::create_queue_with_elements(QueueType::MPSC, values.clone().into_iter());
+  let mut vec_deque_blocking =
+    queue_rs::queue::create_queue_with_elements(QueueType::VecDequeue, values.clone().into_iter()).with_blocking();
+  let mut queue_linked_list_blocking =
+    queue_rs::queue::create_queue_with_elements(QueueType::LinkedList, values.clone().into_iter()).with_blocking();
+  let mut queue_mpsc_blocking =
+    queue_rs::queue::create_queue_with_elements(QueueType::MPSC, values.clone().into_iter()).with_blocking();
   let op = 1;
   group.bench_with_input(BenchmarkId::new("vec_deque", op), &op, |b, i| {
     b.iter(|| vec_deque.poll().unwrap())
@@ -38,6 +58,15 @@ fn poll(c: &mut Criterion) {
   });
   group.bench_with_input(BenchmarkId::new("queue_mpsc", op), &op, |b, i| {
     b.iter(|| queue_mpsc.poll().unwrap())
+  });
+  group.bench_with_input(BenchmarkId::new("vec_deque_blocking", op), &op, |b, i| {
+    b.iter(|| vec_deque_blocking.poll().unwrap())
+  });
+  group.bench_with_input(BenchmarkId::new("linked_list_blocking", op), &op, |b, i| {
+    b.iter(|| queue_linked_list_blocking.poll().unwrap())
+  });
+  group.bench_with_input(BenchmarkId::new("queue_mpsc_blocking", op), &op, |b, i| {
+    b.iter(|| queue_mpsc_blocking.poll().unwrap())
   });
 }
 
